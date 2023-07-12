cc89c8fd5e9b6c71a42aae31af8f16506aeb415a
Jake Friedman <jake.friedman@microsoft.com>
2023-07-12 15:06:39 -0700

    Revert "dismmu"
    This reverts commit 8100b13c327ce137af42a15b0cb52c0639b33461.

--- StandaloneMmPkg/Core/StandaloneMmCore.c
+++ StandaloneMmPkg/Core/StandaloneMmCore.c
@@ -8,7 +8,6 @@
 **/
 
 #include "StandaloneMmCore.h"
-#include <Library/ArmLib.h>
 
 EFI_STATUS
 MmCoreFfsFindMmDriver (
@@ -529,14 +528,6 @@ StandaloneMmMain (
   EFI_MMRAM_DESCRIPTOR            *MmramRanges;
   UINTN                           MmramRangeCount;
   EFI_HOB_FIRMWARE_VOLUME         *BfvHob;
-  
-  ArmDisableMmu ();
-  ArmCleanInvalidateDataCache ();
-    ArmDisableAlignmentCheck ();
-    ArmEnableStackAlignmentCheck ();
-    ArmEnableInstructionCache ();
-    ArmEnableDataCache ();
-ArmEnableMmu();
 
   ProcessLibraryConstructorList (HobStart, &gMmCoreMmst);
 
