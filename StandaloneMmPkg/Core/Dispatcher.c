828137e5f02c0a1ed9d2015ca1f944cbdf49396a
Jake Friedman <jake.friedman@microsoft.com>
2023-07-13 11:44:26 -0700

    back2dispatch

--- StandaloneMmPkg/Core/Dispatcher.c
+++ StandaloneMmPkg/Core/Dispatcher.c
@@ -342,10 +342,11 @@ MmLoadImage (
   //
   ImageContext.ImageAddress += ImageContext.SectionAlignment - 1;
   ImageContext.ImageAddress &= ~((EFI_PHYSICAL_ADDRESS)(ImageContext.SectionAlignment - 1));
-  // Status  = ArmSetMemoryAttributes (ImageContext.ImageAddress, PageCount - EFI_SIZE_TO_PAGES(ImageContext.ImageAddress - DstBuffer), EFI_MEMORY_WB);
-  // ASSERT_EFI_ERROR (Status);
-  Status  = ArmClearMemoryRegionReadOnly (ImageContext.ImageAddress, PageCount - EFI_SIZE_TO_PAGES(ImageContext.ImageAddress - DstBuffer));
+  
+  Status  = ArmClearMemoryRegionReadOnly ((EFI_PHYSICAL_ADDRESS)DstBuffer, EFI_PAGES_TO_SIZE(PageCount));
   ASSERT_EFI_ERROR (Status);
+  DEBUG((DEBUG_INFO, "ArmClearMemoryRegionReadOnly %lx\n",Status));
+
   //
   // Load the image to our new buffer
   //
